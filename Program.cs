917409bc3b77df21f7fbafebcfbb6033b99337c4
Liz <liz.hitches@gmail.com>
2019-05-10 21:13:35 -0400

    test

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Calculate the perimeter and area of various classrooms at grand circus
+
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus Room Detail Generator!");
+            string input3 = "y";
+            while (input3 == "y")
+            {
+                Console.WriteLine("Enter Length: ");
+                string input1 = Console.ReadLine();
+                float length = float.Parse(input1);
+
+                Console.WriteLine("Enter Width: ");
+                string input2 = Console.ReadLine();
+                float width = float.Parse(input2);
+
+                float area = length * width;
+                Console.WriteLine("Area: " + area);
+
+                float perimeter = (length * 2) + (width * 2);
+                Console.WriteLine("Perimeter: " + perimeter);
+
+                Console.WriteLine("Continue? Y/N:");
+                input3 = Console.ReadLine();
+            }
+        }
+    }
+}
